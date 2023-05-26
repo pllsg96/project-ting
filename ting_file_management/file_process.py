00d4642447bcbcff6147d5ce44a970cdac459e03
Pedro Luiz <pedroluizlsg@gmail.com>
2023-05-26 10:53:42 -0300

    feat: implementação remove

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -17,6 +17,10 @@ def process(path_file, instance):
 
 def remove(instance):
     """Aqui irá sua implementação"""
+    if not instance.__len__():
+        return print("Não há elementos", file=sys.stdout)
+    path_file = instance.dequeue()
+    print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instance, position):
