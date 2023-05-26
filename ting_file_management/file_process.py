1a9691db3271aa73b83032553aed3c2eece85ade
Pedro Luiz <pedroluizlsg@gmail.com>
2023-05-26 10:36:31 -0300

    feat: implementação process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file = txt_importer(path_file)
+    file_info = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file),
+        "linhas_do_arquivo": file
+    }
+    if path_file not in instance.fila:
+        instance.enqueue(path_file)
+
+        print(file_info, file=sys.stdout)
 
 
 def remove(instance):
