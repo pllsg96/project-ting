7edb2f5434e136be1c5c57c4f4499cb94fa4dbb3
Pedro Luiz <pedroluizlsg@gmail.com>
2023-05-29 18:26:17 -0300

    comment: comment to refactor

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,21 +1,41 @@
 def exists_word(word, instance):
     result = []
-    while not instance.__len__:
-        file = instance.enqueue()
-        occurrences = []
-        with open(file, 'r') as f:
-            lines = f.readlines()
-            for line_number, line in enumerate(lines, start=1):
-                if word.lower() in line.lower():
-                    occurrences.append({"linha": line_number})
-        if occurrences:
-            result.append({
-                "palavra": word,
-                "arquivo": file,
-                "ocorrencias": occurrences
-            })
+    while not instance.__len__():
+        # file = instance.enqueue()
+        # occurrences = []
+        # with open(file, 'r') as f:
+        #     lines = f.readlines()
+        #     for line_number, line in enumerate(lines, start=1):
+        #         if word.lower() in line.lower():
+        #             occurrences.append({"linha": line_number})
+        # if occurrences:
+        #     result.append({
+        #         "palavra": word,
+        #         "arquivo": file['nome_do_arquivo'],
+        #         "ocorrencias": occurrences
+        #     })
+        return True
     return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    while not instance.__len__():
+        # file = instance.dequeue()
+        # occurrences = []
+        # with open(file, 'r') as f:
+        #     lines = f.readlines()
+        #     for line_number, line in enumerate(lines, start=1):
+        #         if word.lower() in line.lower():
+        #             occurrences.append({
+        #                 "linha": line_number,
+        #                 "conteudo": line.strip()
+        #             })
+        # if occurrences:
+        #     result.append({
+        #         "palavra": word,
+        #         "arquivo": file,
+        #         "ocorrencias": occurrences
+        #     })
+        return True
+    return result
