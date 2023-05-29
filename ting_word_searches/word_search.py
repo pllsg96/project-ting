95d427c86a449c2eb8ac35545bdc72f4f4f7a44a
Pedro Luiz <pedroluizlsg@gmail.com>
2023-05-29 18:07:43 -0300

    feat: implementação função exists_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,20 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    while not instance.__len__:
+        file = instance.enqueue()
+        occurrences = []
+        with open(file, 'r') as f:
+            lines = f.readlines()
+            for line_number, line in enumerate(lines, start=1):
+                if word.lower() in line.lower():
+                    occurrences.append({"linha": line_number})
+        if occurrences:
+            result.append({
+                "palavra": word,
+                "arquivo": file,
+                "ocorrencias": occurrences
+            })
+    return result
 
 
 def search_by_word(word, instance):
