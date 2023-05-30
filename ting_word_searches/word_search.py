3686c3be5be398ed952bf284f8e070f3631ed068
Pedro Luiz <pedroluizlsg@gmail.com>
2023-05-30 07:34:23 -0300

    feat: refactor: search_by_word

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -29,23 +29,34 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    result = []
-    while not instance.__len__():
-        # file = instance.dequeue()
-        # occurrences = []
-        # with open(file, 'r') as f:
-        #     lines = f.readlines()
-        #     for line_number, line in enumerate(lines, start=1):
-        #         if word.lower() in line.lower():
-        #             occurrences.append({
-        #                 "linha": line_number,
-        #                 "conteudo": line.strip()
-        #             })
-        # if occurrences:
-        #     result.append({
-        #         "palavra": word,
-        #         "arquivo": file,
-        #         "ocorrencias": occurrences
-        #     })
-        return True
-    return result
+    occurrences = []
+    if not instance.__len__():
+        return None
+
+    for file in instance.fila:
+        file_processing = txt_importer(file)
+        file_info = {
+            "nome_do_arquivo": file,
+            "qtd_linhas": len(file_processing),
+            "linhas_do_arquivo": file_processing
+        }
+        ocurr = [
+            {"linha": i}
+            for i, lines in enumerate(file_info["linhas_do_arquivo"], 1)
+            if word.lower() in lines.lower()
+        ]
+
+        if ocurr:
+            result = {
+                "palavra": word,
+                "arquivo": file_info["nome_do_arquivo"],
+                "ocorrencias": []
+            }
+            for o in ocurr:
+                conteudo = file_info["linhas_do_arquivo"][o["linha"] - 1]
+                result["ocorrencias"].append({
+                    "linha": o["linha"],
+                    "conteudo": conteudo.rstrip()
+                })
+            occurrences.append(result)
+    return occurrences
